2301d3ffd1c9a064bae4cc6c8420ddcdca69499c
kaede0902 <kaede0920js@gmail.com>
2020-05-02 11:40:27 +0900

    add NAMA logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './fesa-logo-name.svg';
 import './App.css';
 
 function App() {
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Directed by Fukuda Ryoichi</p>
         <a
           className="App-link"
           href="https://reactjs.org"
