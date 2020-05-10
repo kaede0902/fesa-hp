a4d1fb79411bb2f7aa0861795dc0619fbf0b683a
kaede0902 <kaede0920js@gmail.com>
2020-05-10 13:04:42 +0900

    made grid

--- src/App.js
+++ src/App.js
@@ -1,231 +1,60 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col} from 'react-bootstrap';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  '@global': {
-    ul: {
-      margin: 0,
-      padding: 0,
-      listStyle: 'none',
-    },
-  },
-  appBar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbar: {
-    flexWrap: 'wrap',
-  },
-  toolbarTitle: {
-    flexGrow: 1,
-  },
-  link: {
-    margin: theme.spacing(1, 1.5),
-  },
-  heroContent: {
-    padding: theme.spacing(8, 0, 6),
-  },
-  cardHeader: {
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
-  },
-  cardPricing: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'baseline',
-    marginBottom: theme.spacing(2),
-  },
-  footer: {
-    borderTop: `1px solid ${theme.palette.divider}`,
-    marginTop: theme.spacing(8),
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
-    [theme.breakpoints.up('sm')]: {
-      paddingTop: theme.spacing(6),
-      paddingBottom: theme.spacing(6),
-    },
-  },
-}));
-
-const tiers = [
-  {
-    title: '見積もり',
-    description: ['エアコン設置費用', 
-      'アンテナ取り付け費用', 
-      '4k TV 設置', '電話相談お待ちしてます'],
-    buttonText: '見積もりは無料です',
-    buttonVariant: 'outlined',
-  },
-  {
-    title: '工事',
-    subheader: '即日着工',
-    description: [
-      'エアコン取り付け',
-      '冷蔵庫配送',
-      'シーリング取り付け',
-      '電気工事全て',
-    ],
-    buttonText: '仮の文章です',
-    buttonVariant: 'contained',
-  },
-  {
-    title: '修理',
-    description: [
-      '水道点検',
-      'リフォーム',
-      'ガス点検',
-      'コンセント工事',
-    ],
-    buttonText: 'Contact us',
-    buttonVariant: 'outlined',
-  },
-];
-const footers = [
-  {
-    title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations'],
-  },
-  {
-    title: 'Features',
-    description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
-  },
-  {
-    title: 'Resources',
-    description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
-  },
-  {
-    title: 'Legal',
-    description: ['Privacy policy', 'Terms of use'],
-  },
-];
-
-export default function Pricing() {
-  const classes = useStyles();
+function App() {
+  const [row1, setRow1] = useState([
+    1,2,3,4,5,6,7,8,9,10
+  ]);
 
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-            Fukuda Electric System Arts
-          </Typography>
-          <nav>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                水曜定休日
-            </Link>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                西神奈川3-7-1
-            </Link>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-              045-491-4108
-            </Link>
-          </nav>
-          <Button href="#" color="primary" variant="outlined" className={classes.link}>
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-      {/* Hero unit */}
-      <Container maxWidth="sm" component="main" className={classes.heroContent}>
-        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-          私たちにできること
-        </Typography>
-        <Typography variant="h5" align="center" color="textSecondary" component="p">
-          電池一本即日配送．徹底的な速度の質の高い
-          サービスを納得のいくお値段でお届けします．
-        </Typography>
-      </Container>
-      {/* End hero unit */}
-      <Container maxWidth="md" component="main">
-        <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
-            // Enterprise card is full width at sm breakpoint
-            <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
-              <Card>
-                <CardHeader
-                  title={tier.title}
-                  subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{ align: 'center' }}
-                  className={classes.cardHeader}
-                />
-                <CardContent>
-                  <div className={classes.cardPricing}>
-                    <Typography component="h2" variant="h3" color="textPrimary">
-                      {tier.price}
-                    </Typography>
-                    <Typography variant="h6" color="textSecondary">
-                    </Typography>
-                  </div>
-                  <ul>
-                    {tier.description.map((line) => (
-                      <Typography component="li" variant="subtitle1" align="center" key={line}>
-                        {line}
-                      </Typography>
-                    ))}
-                  </ul>
-                </CardContent>
-                <CardActions>
-                  <Button fullWidth variant={tier.buttonVariant} color="primary">
-                    {tier.buttonText}
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-      {/* Footer */}
-      <Container maxWidth="md" component="footer" className={classes.footer}>
-        <Grid container spacing={4} justify="space-evenly">
-          {footers.map((footer) => (
-            <Grid item xs={6} sm={3} key={footer.title}>
-              <Typography variant="h6" color="textPrimary" gutterBottom>
-                {footer.title}
-              </Typography>
-              <ul>
-                {footer.description.map((item) => (
-                  <li key={item}>
-                    <Link href="#" variant="subtitle1" color="textSecondary">
-                      {item}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Grid>
-          ))}
-        </Grid>
-        <Box mt={5}>
-          <Copyright />
-        </Box>
-      </Container>
-      {/* End footer */}
-    </React.Fragment>
+    <Container className='my-5'>
+      <Row>
+        <Col className='py-5 bg-danger' 
+          xs={3}>1 to 3, top-logo </Col>
+        <Col className='py-5 bg-secondary' 
+          xs={2}>4 to 5, big-logo</Col>
+        <Col className='py-5 bg-danger' 
+          xs={7}>6 to 13, text... </Col>
+      </Row>
+      <Row>
+        <Col className='py-3 bg-info' 
+          xs={3}>HOME BTN</Col>
+        <Col className='py-3 bg-danger' 
+          xs={2}>BLANK</Col>
+        <Col className='py-3 bg-secondary' 
+          xs={7}>BLANK</Col>
+      </Row>
+      <Row>
+        <Col className='py-3 bg-info' 
+          xs={3}>NEW INFO</Col>
+        <Col className='py-3 bg-secondary' 
+          xs={2}>SHOP 1 NAME</Col>
+        <Col className='py-3 bg-danger' 
+          xs={7}>SHOP 1 TEL</Col>
+      </Row>
+      <Row>
+        <Col className='py-3 bg-info' 
+          xs={3}>ABOUT WORK</Col>
+        <Col className='py-3 bg-danger' 
+          xs={2}>BLANK</Col>
+        <Col className='py-3 bg-secondary' 
+          xs={7}>BLANK</Col>
+      </Row>
+      <Row>
+        <Col  className='py-5' xs={3}>1 </Col>
+        <Col>4 </Col>
+        <Col>5 </Col>
+        <Col>6 </Col>
+        <Col>7 </Col>
+        <Col>8 </Col>
+        <Col>9 </Col>
+        <Col>10 </Col>
+        <Col>11 </Col>
+        <Col>12 </Col>
+        <Col>13 </Col>
+      </Row>
+    </Container>
   );
 }
+export default App;
