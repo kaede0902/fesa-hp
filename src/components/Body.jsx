6fed6f4b483f82bcc7b1735d1c31403c49cb96ec
kaede0902 <kaede0920js@gmail.com>
2020-05-23 16:12:00 +0900

    done card

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
+import { Card, CardGroup, } from 'react-bootstrap/';
+import shops from '../ShopDataList';
 
 const Body = () => {
   const bodyText = 
   `
 地デジアンテナの設置、エアコン取り付け、スイッチ修理、コンセントの増設、お買い上げいただいた商品の配達などお引き受け致しております.
 高所の蛍光灯や電球の交換など，お気軽にお電話ください！
-  `
+  `;
+
+
+  const ShopCards = shops.map((item, key) => 
+    <div
+      key={item.tel} className='d-flex flex-row'
+    >
+        <Card 
+          style = {{ width: '24rem' }}
+          border = 'primary'
+          className='mx-2 my-2'
+        >
+          <Card.Body>
+            <Card.Title>
+              フクダデンキ{item.name}店
+            </Card.Title>    
+            <Card.Text
+              className = 'text-danger'
+            >
+              {item.tel}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+    </div>
+  );
 
   return (
+    <div>
     <h2 className='px-4 py-4'>{bodyText}</h2>
+    <CardGroup
+      className='px-4 py-4 align-item-center justify-content-center'
+    >
+      {ShopCards}
+    </CardGroup>
+    </div>
   );
 }
 export default Body;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,30 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Row, Col} from 'react-bootstrap';
 import '../css/body.css'
+import shops from '../ShopDataList';
 
 const Footer = () => {
-  const shops = [
-    {
-      name: '妙蓮寺',
-      adress: '横浜市港北区菊名1-4-1',
-      tel: '045-421-5752',
-    },
-    {
-      name: '市が尾',
-      adress: '横浜市青葉区市ケ尾町1064-5',
-      tel: '045-971-5116',
-    },
-    {
-      name: '大倉山',
-      adress: '横浜市港北区大倉山3-1-22',
-      tel: '045-531-2170',
-    },
-    {
-      name: '六角橋',
-      adress: '	横浜市神奈川区西神奈川3-7-1 池田ビル1F',
-      tel: '045-491-4108',
-    }
-  ];
   const shopItems = shops.map((item, key) =>
     <div key={item.tel}>
       フクダデンキ{item.name}店: {item.adress}: 
