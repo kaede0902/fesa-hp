86699c7af09bcfff165709ca5627c956cd5f659e
kaede0902 <kaede0920js@gmail.com>
2020-06-13 16:09:17 +0900

    made Header responsive

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
 import routes from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap';
-import SideMenu from './components/SideMenu';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './css/body.css';
@@ -16,32 +15,25 @@ let loading = () => <div>Loading...</div>
     <Container className=''>
       <Header />
       <Row className='leftside'>
-        <Col className='' 
-          xs={0} md={2}>
-          <SideMenu />
-        </Col>
-        <Col className='bodyText' 
-          xs={12} md={10} >
-            <Router>
-            <Suspense fallback={loading}>
-            <Switch>
-             {
-              routes.map((route, index) => (
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  name={route.name}
-                  render={props => 
-                    (<route.component {...props} />)}
-                />
-              ))
-             }
-             <Route render={() => <h1>not found!.</h1>} />
-            </Switch>
-            </Suspense>
-            </Router>
-        </Col>
+        <Router>
+        <Suspense fallback={loading}>
+        <Switch>
+         {
+          routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              name={route.name}
+              render={props => 
+                (<route.component {...props} />)}
+            />
+          ))
+         }
+         <Route render={() => <h1>not found!.</h1>} />
+        </Switch>
+        </Suspense>
+        </Router>
       </Row>
       <Footer />
     </Container>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Row, Col} from 'react-bootstrap';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, NavDropDown} from 'react-bootstrap';
 import logo from '../img/narrow-logo.svg';
 import '../css/body.css'
+import NavData from '../NavDataList'
 
 
 const Header = () => {
   const headerText = `
     神奈川区のお困りごとはお任せ下さい．
   `;
+  const navItems = NavData.map((item, key) => 
+    <Nav.Link 
+      href={item.link} key={key}
+    >{item.name}</Nav.Link>
+  );
+
+
   return (
       <Row className=''>
-      <Navbar bg="" className='m-0'>
+      <Navbar collapseOnSelect expand = 'lg'>
         <Navbar.Brand href="./" >
           <img
             src={logo}
@@ -21,8 +29,15 @@ const Header = () => {
             alt="React Bootstrap logo"
           />
         </Navbar.Brand>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
+
+      
+        <Navbar.Toggle 
+          aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className='mr-auto'>
+            {navItems}
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       </Row>
   );
